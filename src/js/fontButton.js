37fa86802cf5397dee2d4ac5b23713aff795b851
mogicacao <raffaelmogicato@gmail.com>
2022-05-05 14:42:07 +0200

    refactored button

---
+++ src/js/fontButton.js
@@ -0,0 +1,33 @@
+function increaseSize(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    let style = window.getComputedStyle(document.body).getPropertyValue('font-size');
+    let current_size = parseFloat(style);
+    let new_size = current_size + 2;
+
+    $('body').css('font-size', new_size);
+}
+
+function decreaseSize(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    let style = window.getComputedStyle(document.body).getPropertyValue('font-size');
+    let current_size = parseFloat(style);
+    let new_size = current_size - 2;
+
+    $('body').css('font-size', new_size);
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    document
+        .getElementById('font-increase-button')
+        .addEventListener('click', increaseSize, false);
+
+    document
+        .getElementById('font-decrease-button')
+        .addEventListener('click', decreaseSize, false);
+
+    }, false);
