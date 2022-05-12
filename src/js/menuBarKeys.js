6d8be71bde1ea3022323dca3e2d87ecb821eb38d
mogicacao <raffaelmogicato@gmail.com>
2022-05-12 16:23:07 +0200

    added behaviour of space-key and Tab+Shift for navigation bar

--- src/js/menuBarKeys.js
+++ src/js/menuBarKeys.js
@@ -1,30 +1,40 @@
 function keydown(event) {
-
     if (event.key === "Escape") {
         event.stopPropagation();
         event.preventDefault();
         closeMenu();
     }
 
-    if (event.key === "Tab") {
+    if (event.shiftKey && event.keyCode === 9) {
+        let currentDropDownButton = event.target;
+        if (currentDropDownButton.className === "dropdown-item"){
+            let first_child = currentDropDownButton.parentNode.parentNode.firstElementChild.firstElementChild
+            if (currentDropDownButton.textContent === first_child.textContent){
+                 closeMenu();
+            }
+        }
+    }
+    else if (event.key === "Tab") {
         let currentDropDownButton = event.target;
         if (currentDropDownButton.className === "dropdown-item"){
             let last_child = currentDropDownButton.parentNode.parentNode.lastElementChild.lastElementChild
+
             if (currentDropDownButton.textContent === last_child.textContent){
                 closeMenu();
             }
         }
     }
-
     if (event.keyCode === 32) {
-        // simulate key strokes
-        let e = jQuery.Event("keydown");
-        e.which = 13
-        e.keyCode = 13
-        $(event.target).trigger(e)
-        console.log(e)
+        event.stopPropagation();
+        event.preventDefault();
+        let currentDropDownButton = event.target;
+        if (currentDropDownButton.className === "dropdown-item" || currentDropDownButton.className === "nav-link dropdown-toggle" ){
+            currentDropDownButton.click()
+        }
     }
 }
+
+
 
 function closeMenu(){
     let dropDownMenus = document.querySelectorAll('#nav-bar-content .dropdown .dropdown-menu');
